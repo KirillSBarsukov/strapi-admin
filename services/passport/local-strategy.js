859d516765390cc7434c70c9fd4fe65abf539476
Kirill Barsukov <kirillsbarsukov@gmail.com>
2021-04-12 18:13:11 -0400

    test

--- services/passport/local-strategy.js
+++ services/passport/local-strategy.js
@@ -12,6 +12,7 @@ const createLocalStrategy = strapi => {
       session: false,
     },
     (email, password, done) => {
+      console.log("lkadsf")
       return strapi.admin.services.auth
         .checkCredentials({ email, password })
         .then(([error, user, message]) => done(error, user, message))
